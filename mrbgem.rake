903bfb55728c0a1607627c5b29762452a2ea314d
Satoshi Kojima <skoji@mac.com>
2019-07-21 16:00:42 +0900

    implement Loadavg.get

--- mrbgem.rake
+++ mrbgem.rake
@@ -1,4 +1,6 @@
 MRuby::Gem::Specification.new('mruby-loadavg') do |spec|
   spec.license = 'MIT'
   spec.authors = 'Satoshi Kojima'
+
+  spec.add_dependency 'mruby-io'
 end
--- mrblib/mrb_loadavg.rb
+++ mrblib/mrb_loadavg.rb
@@ -1,5 +1,14 @@
 class Loadavg
-  def bye
-    self.hello + " bye"
+  def self.get
+    loadavg = File.read('/proc/loadavg').split
+    runnable_entities, existing_entities = loadavg[3].split('/', 2)
+    {
+      one_min_avg: loadavg[0].to_f,
+      five_min_avg: loadavg[1].to_f,
+      fifteen_min_avg: loadavg[2].to_f,
+      runnable_entities: runnable_entities.to_i,
+      existing_entities: existing_entities.to_i,
+      last_pid: loadavg[4].to_i
+    }
   end
 end
